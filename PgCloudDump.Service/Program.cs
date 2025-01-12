c54c8a0dd2c661e0b5bf2f401255c3584c7249f0
Settler <104102+Settler@users.noreply.github.com>
2025-01-12 12:33:19 +0300

    Enabled swagger

--- PgCloudDump.Service/Program.cs
+++ PgCloudDump.Service/Program.cs
@@ -33,11 +33,8 @@ try
 
     var app = builder.Build();
 
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseAuthorization();
 
